3fd4662a7ac8263dd98d47e8819c22ca86e41665
gohilankita <ankitagohil9925@gmail.com>
2024-07-27 15:37:03 +0530

    setData function
    setData function for setting up individual cells

--- sudoku.c
+++ sudoku.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 void initialize(int row, int col, int x[row][col]);
 void display(int row, int col, int x[row][col]);
-
+void setData(int row, int col, int x[row][col], int r, int c, int data);
 void main()
 {
     int a[4][4];
     initialize(4, 4, a);
-    a[0][0] = 2;
-    a[1][1] = 1;
-    a[1][3] = 2;
-    a[2][2] = 3;
-    a[3][3] = 4;
+    setData(4, 4, a, 0, 0, 2);
+    setData(4, 4, a, 1, 1, 1);
+    setData(4, 4, a, 1, 3, 2);
+    setData(4, 4, a, 2, 2, 3);
+    setData(4, 4, a, 3, 3, 4);
     display(4, 4, a);
 }
 
@@ -36,4 +36,8 @@ void display(int row, int col, int x[row][col])
         }
         printf("\n");
     }    
+}
+void setData(int row, int col, int x[row][col], int r, int c, int data)
+{
+    x[r][c] = data;
 }
