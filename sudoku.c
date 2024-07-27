17c624b0e21868d0b69c7805b04388ef817783db
gohilankita <ankitagohil9925@gmail.com>
2024-07-27 15:05:07 +0530

    Basic display & store of sudoku

--- sudoku.c
+++ sudoku.c
@@ -2,5 +2,17 @@
 
 void main()
 {
-    printf("Hello, World\n");
+    int i,j;
+    int a[4][4] = {{2, 0, 0, 0},
+                   {0, 1, 0, 2},
+                   {0, 0, 3, 0},
+                   {0, 0, 0, 4}};
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
 }
