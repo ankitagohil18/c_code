e0852662e3bc42791bd1f63a70daa4b3b0bd2d7d
gohilankita <ankitagohil9925@gmail.com>
2024-07-27 15:25:58 +0530

    Initialize function

--- sudoku.c
+++ sudoku.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
+void initialize(int row, int col, int x[row][col]);
 
 void main()
 {
     int i,j;
     int a[4][4];
-    for (i = 0; i < 4; i++)
-    {
-        for (j = 0; j < 4; j++)
-        {
-            a[i][j] = 0;
-        }
-    }
+    initialize(4, 4, a);
     a[0][0] = 2;
     a[1][1] = 1;
     a[1][3] = 2;
@@ -23,5 +18,17 @@ void main()
             printf("%d ", a[i][j]);
         }
         printf("\n");
+    }
+}
+
+void initialize(int row, int col, int x[row][col])
+{
+    int i, j;
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            x[i][j] = 0;
+        }
     }
 }
