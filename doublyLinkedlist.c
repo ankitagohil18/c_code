b123cd0a1063ab40258664bf424648028021691e
gohilankita <ankitagohil9925@gmail.com>
2025-07-27 12:18:34 +0530

    Armstrong Number

--- doublyLinkedlist.c
+++ doublyLinkedlist.c
@@ -10,9 +10,11 @@ struct node {
 struct node *create(int);
 void display(struct node *);
 void insert_last(struct node* , int);
-int insert_starting(struct node*, int);
+struct node *insert_starting(struct node*, int);
 int inert_after(struct node*,int , int );
 int insert_before(struct node*, int, int);
+void delete_first(struct node **);
+// void delete_last(struct node *);
 
 
 int main(){
@@ -26,9 +28,11 @@ int main(){
     p = insert_starting(p,50);
     insert_before(p,10,60);
     inert_after(p,50,80);
+    
+    delet_first(&p);
     display(p);
 }
-
+// update
 struct node *create(int data)
 {
     struct node *new_node = (struct node*)malloc(sizeof(struct node));
@@ -56,19 +60,21 @@ void display(struct node *x)
     }   
 }
 
-int insert_starting(struct node* x, int data)
+struct node *insert_starting(struct node* x, int data)
 {   
     struct node *new_node;
     new_node = create(data);
     if(x == NULL){
         x = new_node;
+        return new_node;
     }
     
     (*new_node).next = x;
     (*x).prev = new_node;
     (*new_node).prev = NULL;
     x = new_node;
-
+    return new_node;
+    
 }
 
 int inert_after(struct node* x, int key, int data)
@@ -106,3 +112,28 @@ int insert_before(struct node *x, int key, int data)
      (*new_node).prev = x;
      (*x).next = new_node;
 }
+
+void delete_first(struct node **head) 
+{
+    // If the list is already empty
+    if (*head == NULL) {
+        printf("List is already empty.\n");
+        return;
+    }
+
+    struct node *temp = *head;      // Save the current head
+    *head = (*head)->next;          // Move head to the next node
+
+    if (*head != NULL) {
+        (*head)->prev = NULL;       // Set new head's prev to NULL
+    }
+
+    free(temp);                     // Free the old head
+    printf("First node deleted.\n");
+}
+
+
+// void delete_last(struct node *x)
+// {
+    
+// }
