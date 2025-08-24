15b5f5f0eadfb843375da7b490708fda12d0d868
gohilankita <ankitagohil9925@gmail.com>
2025-08-25 00:59:12 +0530

    pyramid of number implemented

---
+++ patterns/pattern6.c
@@ -0,0 +1,25 @@
+/*
+1
+12
+123
+1234
+*/
+#include<stdio.h>
+int main(){
+    for(int i = 1; i<=4;i++){
+        for(int j = 1; j <= i;j++){
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    int n = 4;
+    int a = n;
+    for(int i = 1; i <= n;i++){
+        for(int j = 1; j<=a;j++){
+            // j=n+1-1 or n=a ||j+i=n+1
+            printf("%d",j);
+        }
+        a--;
+        printf("\n");
+    }
+}
